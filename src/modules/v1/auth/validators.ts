453cbb4fc78d0cb75fd693691142229ded5faf85
bhumit070 <ganatrabhoomit070@gmail.com>
2024-05-19 16:42:37 +0530

    fix: typo

--- src/modules/v1/auth/validators.ts
+++ src/modules/v1/auth/validators.ts
@@ -1,15 +1,17 @@
 import { z } from 'zod';
 
-
 export const loginSchema = z.object({
-	email: z.string().email(),
-	password: z.string().min(8).max(30).regex(/^[a-zA-Z0-9]*$/)
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8)
+    .max(30)
+    .regex(/^[a-zA-Z0-9]*$/),
 });
 
 export const registerSchema = z.object({
-	name: z.string().min(2).max(4),
-	...loginSchema.shape,
-	data: z.null()
+  name: z.string().min(2).max(4),
+  ...loginSchema.shape,
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
